0bb669b5edab47e1ee9c01b4ba3130ecca4e740b
Kamila <62843248+kamilalfc@users.noreply.github.com>
2021-03-18 16:06:58 +0000

    Add files via upload

--- oxford_02.21/app.js
+++ oxford_02.21/app.js
@@ -9,12 +9,15 @@ function animateSlides() {
 	sliders.forEach((slide, index, slides) => {
 		const revealText = slide.querySelector(".reveal-text");
 		const img = slide.querySelectorAll(".photo-cell");
+		const desc = slide.querySelectorAll("p,h2");
 
 		const slideTl = gsap.timeline({
 			defaults: { duration: 1, ease: "power2.inOut" },
 		});
 		slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" });
-		slideTl.fromTo(img, { scale: 0.9 }, { scale: 1 }, "-=1");
+		slideTl.fromTo(img, { scale: 0.7 }, { scale: 1 }, "-=1");
+		slideTl.fromTo(desc, { opacity: 0 }, { opacity: 1 }, "-=1");
+
 		slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
 
 		slideScene = new ScrollMagic.Scene({
@@ -55,7 +58,7 @@ previews.forEach((preview) => {
 		original.classList.add("open");
 
 		const originalSrc = preview.getAttribute("data-original");
-		original.src = `../img/oxford/${originalSrc}`;
+		original.src = `../img/london_02.21/${originalSrc}`;
 	});
 });
 modal.addEventListener("click", (e) => {
