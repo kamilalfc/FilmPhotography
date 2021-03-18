b1ceabc25003055f9187f58479461fe919ef556e
Kamila <62843248+kamilalfc@users.noreply.github.com>
2021-03-18 16:05:38 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ function animateSlides() {
 	sliders.forEach((slide, index, slides) => {
 		const revealImg = slide.querySelector(".reveal-img");
 		const img = slide.querySelector("img");
-		const revealText = slide.querySelector(".reveal-text");
+		const text = slide.querySelector(".intro-text");
+		const desc = slide.querySelectorAll("p,h2");
 
 		const slideTl = gsap.timeline({
 			defaults: { duration: 1, ease: "power2.inOut" },
@@ -18,8 +19,9 @@ function animateSlides() {
 
 		slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
 		slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
-		slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+		slideTl.fromTo(text, { opacity: 0 }, { opacity: 1 }, "-=0.4");
 		slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+		slideTl.fromTo(desc, { opacity: 0 }, { opacity: 1 }, "-=1");
 
 		slideScene = new ScrollMagic.Scene({
 			triggerElement: slide,
